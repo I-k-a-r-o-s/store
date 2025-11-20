2f137c0c969f795d6a7a032f512af38735b5c301
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2025-11-20 16:18:52 +0530

    replaced the hardcoded port with port from .env file

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,9 @@ app.use(express.json()); //allows to accept json data in the req.body
 
 app.use("/api/products", router);
 
-app.listen(5000, () => {
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
   connectDB();
-  console.log("Server started");
+  console.log("Server started at port:"+port);
 });
