75940e2662351c364e4df2159ed48e87318d3e99
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2025-11-24 17:37:26 +0530

    Built navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,27 @@
+import { ShoppingCart, SquarePlus } from "lucide-react";
+import { Link } from "react-router";
+import Darkmode from "./Darkmode";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
-export default Navbar
+    <div className="navbar bg-base-200 shadow-sm">
+      <div className="flex-1">
+        <Link to={"/"} className="btn btn-ghost text-xl">
+          Store
+          <ShoppingCart />
+        </Link>
+      </div>
+      <div className="navbar-end">
+        <Link to="/create">
+          <button className="btn btn-circle btn-ghost mx-2">
+            <SquarePlus />
+          </button>
+        </Link>
+        <button className="btn btn-circle btn-ghost mx-2">
+          <Darkmode />
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
