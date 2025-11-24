13ba60c21664e1e59b9c6f3a68956d4e3e894f4b
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2025-11-24 15:37:02 +0530

    added routing to pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,26 @@
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Navbar from "./components/Navbar";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/create",
+    element: <CreatePage />,
+  },
+]);
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <div>
+      <Navbar />
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+export default App;
